1ac23235af7813e6e9a562995638f77f855b0dbe
Dnyaneshwari Ambadas Shinde <dnyaneshwari@Dnyaneshwaris-MacBook-Air.local>
2024-08-12 13:35:56 +0530

    update

--- apps/backend/src/Util.ts
+++ apps/backend/src/Util.ts
@@ -2,11 +2,11 @@ import * as child_process from "node:child_process";
 
 
 export const mountNfs1 = async (path: string) => {
-    await execPromise(`mount_nfs -o nolocks,vers=3,tcp,rsize=131072,actimeo=120,port=11111,mountport=11111 localhost:/ ${path}`)
+    await execPromise(`mount_nfs -o nolocks,vers=3,tcp,rsize=131072,actimeo=120,port=6382,mountport=6379 localhost:/ ${path}`)
 }
 
 export const mountNfs2 = async (path: string) => {
-    await execPromise(`mount_nfs -o nolocks,vers=3,tcp,rsize=131072,actimeo=120,port=11112,mountport=11112 localhost:/ ${path}`);
+    await execPromise(`mount_nfs -o nolocks,vers=3,tcp,rsize=131072,actimeo=120,port=6382,mountport=6379 localhost:/ ${path}`);
 }
 
 const execPromise = (command: string): Promise<{ stdout: string, stderr: string }> => {
